ead59742299531fa628ab78d6a72a390014fb52a
kwlucas <lucaswkyle@gmail.com>
2022-12-06 13:36:19 -0700

    Crated thought model

---
+++ models/Thought.js
@@ -0,0 +1,70 @@
+const { Schema, model, Types } = require('mongoose');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: setDate,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [
+        reactionSchema
+    ],
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false,
+}
+);
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdDate: {
+            type: Date,
+            default: Date.now,
+            get: setDate,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        _id: false,
+    }
+);
+
+function setDate(timestamp) {
+    return timestamp.toLocaleString();
+}
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
