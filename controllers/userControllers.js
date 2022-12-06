5dde44a007098c6abc11d4e56a56d88d7c4c3826
kwlucas <lucaswkyle@gmail.com>
2022-12-06 13:56:18 -0700

    Created get user request

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -9,5 +9,15 @@ module.exports = {
             console.error(err);
             res.status(500).json(err);
         }
+    },
+
+    async getUser(req, res) {
+        try {
+            const userData = await User.findById(req.params.userId).populate('thoughts').populate('friends');
+            userData ? res.status(200).json(userData) : res.status(404).json({ message: 'No data found!' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
     }
 }
