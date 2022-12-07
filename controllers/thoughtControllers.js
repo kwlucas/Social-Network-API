e399b630a907a74c9f4d71680b2afd66a96a0ccc
kwlucas <lucaswkyle@gmail.com>
2022-12-06 22:48:40 -0700

    Created request to get a single thought

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -10,4 +10,14 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    async getThought(req, res) {
+        try {
+            const thoughtData = await Thought.findById(req.params.thoughtId);
+            thoughtData ? res.status(200).json(thoughtData) : res.status(404).json({ message: 'No data found!' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
 }
