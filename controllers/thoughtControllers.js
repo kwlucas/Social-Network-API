d4597501b2b6dcbde1c2200b14a8ca01a1154c15
kwlucas <lucaswkyle@gmail.com>
2022-12-06 22:39:45 -0700

    Created request to get all thoughts

---
+++ controllers/thoughtControllers.js
@@ -0,0 +1,13 @@
+const { Thought } = require("../models");
+
+module.exports = {
+    async getAllThoughts(req, res) {
+        try {
+            const thoughtData = await Thought.find();
+            thoughtData ? res.status(200).json(thoughtData) : res.status(404).json({ message: 'No data found!' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
+}
